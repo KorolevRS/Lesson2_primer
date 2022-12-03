b400a6d6308d1815953f0ceb5b1f5692454bf56f
Roman <romeoone@mail.ru>
2022-12-03 16:48:50 +0300

    закончили лекцию2

---
+++ Example_004/Program.cs
@@ -0,0 +1,14 @@
+﻿int[] array = { 1, 56, 21, 44, 2, 32, 85, 14, 12 };
+int n = array.Length;
+int find = 12;
+int index = 0;
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    
+    index++;
+}
