9238c980102cf1d09dd23447d472ca37e7c57246
Roman <romeoone@mail.ru>
2022-12-07 20:53:31 +0300

    Добавил программу, которая принимает на вход число и выдает количество цифр: два способа

--- Seminar_010/Program.cs
+++ Seminar_010/Program.cs
@@ -4,12 +4,12 @@ int a;
 Console.Write($"Введите число: ");
 int.TryParse(Console.ReadLine()!, out a);
 
-// Console.WriteLine($"Колличество цифр в числе {a} = {a.ToString().Length} ");
+Console.WriteLine($"Колличество цифр в числе {a} = {a.ToString().Length} ");
 
-int k = 0;
-while (a > 0)
-{
-    a /= 10;
-    k++;
-}
-Console.Write(k);
+// int k = 0;
+// while (a > 0) //Математический способ решения задачи
+// {
+//     a /= 10;
+//     k++;
+// }
+// Console.WriteLine(k);
