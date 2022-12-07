939cf47fc213ac5ddbe086fc68db7bc23ddc3e25
Roman <romeoone@mail.ru>
2022-12-07 20:11:06 +0300

    добавил задачу по преобразование числа в текст и работа с массивом букв как числовые значения

---
+++ Seminar_007/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+int[] array = new int[5];
+
+for (int i = 0; i < array.Length; i++)
+    array[i] = new Random().Next(10);
+Print(array);
+
+int sum = 0;
+for (int i = 0; i < array.Length / 2; i++) //с 8 по 16 строку подсчитывает сумму элементов массива
+// используя зеркальные элементы
+{
+    sum += array[i];
+    sum += array[array.Length - i - 1];
+}
+if (array.Length % 2 != 0)
+    sum += array[array.Length / 2];
+Console.WriteLine(sum);
+
+void Print(int[] arr) //Вывод массива на экран
+{
+    for (int i = 0; i < array.Length; i++)
+        Console.Write($"{array[i]} ");
+    Console.WriteLine();
+}
---
+++ Seminar_008/Program.cs
@@ -0,0 +1,13 @@
+﻿// Sпреобразование числа в текст и работа с массивом букв как числовые значения
+int a;
+Console.Write($"Введите число: ");
+string user = Console.ReadLine()!; //считал число
+int.TryParse(user, out a); //убедились что это число а
+user = a.ToString(); // перобразовали число в текст
+int[] array = new int[user.Length]; //объявили численный массив с длинной строки user
+
+for (int i = 0; i < array.Length; i++) // присваиваем каждомо элементу числового масива символ из строки
+    array[i] = int.Parse(user[i].ToString());// присваиваем каждому элементу числового массива символ из строки
+
+for (int i = 0; i < array.Length; i++) //вывод массива на экран
+    Console.WriteLine($"{array[i]} ");
