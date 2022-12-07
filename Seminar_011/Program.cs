6e18abb10df86df1f635233b659643070135be49
Roman <romeoone@mail.ru>
2022-12-07 21:06:26 +0300

    Добавил программуапишите программу, //  которая выводит массив из 8 элементов, заполненный нулями и единицами в случайном порядке

---
+++ Seminar_011/Program.cs
@@ -0,0 +1,11 @@
+﻿// // Задача 30. Напишите программу,
+//  которая выводит массив из 8 элементов, 
+//  заполненный нулями и единицами в случайном порядке
+int[] array = new int[8];
+
+for (int i = 0; i < 8; i++)
+    array[i] = new Random().Next(2);
+
+for (int i = 0; i < 8; i++)
+    Console.Write($"{array[i]} ");
+Console.WriteLine();
