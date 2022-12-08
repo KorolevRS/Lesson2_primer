c31c9deb97cb066e85c36d6d09f63b9a885a81f4
Roman <romeoone@mail.ru>
2022-12-08 22:44:32 +0300

    Частично написал код из семинара 08.12.2022

---
+++ Seminar_012/Program.cs
@@ -0,0 +1,30 @@
+﻿// Программа которая находит 
+// заданное число в рандомном массиве
+int[] array = new int[5];
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(0, 10);
+}
+Print(array);
+Console.WriteLine();
+int a;
+Console.WriteLine($"Введите число: ");
+int.TryParse(Console.ReadLine()!, out a);
+
+Find(a, array);
+
+void Find(int b, int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        if (array[i] == b)
+        {
+            Console.WriteLine($"да");
+            return; //можно написать break
+        }
+    Console.WriteLine("Нет");
+}
+void Print(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+        Console.Write($"{arr[i]} ");
+}
---
+++ Seminar_013/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задайте массив из 12 элементов, заполненный
+// случайными числами из промежутка [-9,9]. 
+//  Найдите сумму отрицательных и 
+//  положительных элементов массива
+
+int[] array = new int[12];
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(-9, 9);
+}
+Print(array);
+Console.WriteLine();
+
+int neg = 0;
+int pos = 0;
+
+for (int i = 0; i < array.Length; i++)
+    if (array[i] == b)
+    {
+        Console.WriteLine($"да");
+        return; //можно написать break
+    }
+Console.WriteLine("Нет");
